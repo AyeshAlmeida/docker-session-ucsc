5a8621dec80cef07e166bfebdf9c2dfc686e90d0
ayeshs <ayeshs@hsenidmobile.com>
2020-06-09 17:19:25 +0530

    Fix: Minor refactoring.

--- src/test/java/hms/sessions/dockersessionucsc/DockerSessionUcscApplicationTests.java
+++ src/test/java/hms/sessions/dockersessionucsc/DockerSessionUcscApplicationTests.java
@@ -3,10 +3,10 @@ package hms.sessions.dockersessionucsc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DockerSessionUcscApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
